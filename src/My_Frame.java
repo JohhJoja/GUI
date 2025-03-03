dca3c684fef9f706c9a330632a3087325837ae1d
Dima Eliseev <08022002dimaeliseew@gmail.com>
2025-03-03 15:53:07 +0300

    first frame

---
+++ src/My_Frame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class My_Frame extends JFrame {
+
+    My_Frame(){
+
+        setVisible(true);
+        setSize(100,100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
