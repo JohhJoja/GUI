c2584bae4d725644e458a82ab61050f0bd9ed133
Dima Eliseev <08022002dimaeliseew@gmail.com>
2025-03-03 16:30:29 +0300

    create my frame and label

--- src/My_Frame.java
+++ src/My_Frame.java
@@ -1,13 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class My_Frame extends JFrame {
 
     My_Frame(){
 
         setVisible(true);
-        setSize(100,100);
+        setSize(400,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("My frame!");
+       // ImageIcon img = new ImageIcon("");
+       // setIconImage(img);
+        getContentPane().setBackground(Color.CYAN);
+
+//        JLabel label = new JLabel("Bro, darova");
+//        ImageIcon img = new ImageIcon("");
+//        label.setIcon(img);
+//        label.setHorizontalTextPosition(JLabel.CENTER);
+//        label.setVerticalTextPosition(JLabel.TOP);
+//        label.setForeground(Color.GREEN);
+//        label.setFont(new Font("MV Boli", Font.PLAIN, 20));
+//        label.setIconTextGap(10);
+//        label.setBackground(Color.BLACK);
+//        label.setOpaque(true);
+//        label.setBorder(BorderFactory.createLineBorder(Color.BLUE, 10));
+//        label.setHorizontalAlignment(JLabel.CENTER);
+//        label.setVerticalAlignment(JLabel.CENTER);
+       // label.setBounds(0,0,250,250);
+
+       // setLayout(null);
+  //      add(label);
+
+        
 
     }
-
 }
