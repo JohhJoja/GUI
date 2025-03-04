f3b809f8d98013e2893be30e4619711d67b049ef
Dima Eliseev <08022002dimaeliseew@gmail.com>
2025-03-04 10:15:46 +0300

    BorderLayout

--- src/My_Frame.java
+++ src/My_Frame.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class My_Frame extends JFrame implements ActionListener {
+public class My_Frame extends JFrame  {
 
     JButton button;
 
@@ -17,6 +17,33 @@ public class My_Frame extends JFrame implements ActionListener {
        // setIconImage(img);
         getContentPane().setBackground(Color.CYAN);
 
+        setLayout(new BorderLayout());
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        panel1.setBackground(Color.RED);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.orange);
+        panel4.setBackground(Color.yellow);
+        panel5.setBackground(Color.blue);
+
+        panel1.setPreferredSize(new Dimension(100,100));
+        panel2.setPreferredSize(new Dimension(100,100));
+        panel3.setPreferredSize(new Dimension(100,100));
+        panel4.setPreferredSize(new Dimension(100,100));
+        panel5.setPreferredSize(new Dimension(100,100));
+
+        add(panel1, BorderLayout.WEST);
+        add(panel2, BorderLayout.EAST);
+        add(panel3, BorderLayout.SOUTH);
+        add(panel4, BorderLayout.CENTER);
+        add(panel5, BorderLayout.NORTH);
+
+        
 //        JLabel label = new JLabel("Bro, darova");
 //        ImageIcon img = new ImageIcon("");
 //        label.setIcon(img);
@@ -34,7 +61,7 @@ public class My_Frame extends JFrame implements ActionListener {
 
   //      add(label);
 
-        setLayout(null);
+    //    setLayout(null);
 //
 //        JPanel panel = new JPanel();
 //        panel.setBackground(Color.GREEN);
@@ -43,21 +70,24 @@ public class My_Frame extends JFrame implements ActionListener {
 //
 //        add(panel);
 
-        button = new JButton("Click!");
-        button.setBounds(200,100,100,50);
-        button.addActionListener(this);
-        button.setFocusable(false);
-        button.setBackground(Color.magenta);
-        button.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
+//        button = new JButton("Click!");
+//        button.setBounds(200,100,100,50);
+//        button.addActionListener(this);
+//        button.setFocusable(false);
+//        button.setBackground(Color.magenta);
+//        button.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
+//
+//        add(button);
+//    }
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        if (e.getSource()==button){
+//            System.out.println("Click!");
+//            button.setEnabled(false);
+//        }
 
-        add(button);
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==button){
-            System.out.println("Click!");
-            button.setEnabled(false);
-        }
+
     }
 }
